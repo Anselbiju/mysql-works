2869d4cf0fa8a1565725067f29198d565046d4a0
Anselbiju <anselbiju5@gmail.com>
2024-10-23 17:05:08 +0530

    Update DDL Comands.sql

--- DDL Comands.sql
+++ DDL Comands.sql
@@ -2,3 +2,23 @@ use school;
 
 desc STUDENT;
 
+insert into STUDENT values
+(1,'ansel',40,'A'),
+(2,'ans',45,'A+'),
+(3,'ebin',48,'A+');
+
+select * from STUDENT;
+
+alter table STUDENT add column contact int;
+
+select * from STUDENT;
+
+alter table STUDENT drop column Grade;
+
+rename table STUDENT to CLASSTEN;
+
+truncate table CLASSTEN;
+
+drop table CLASSTEN;
+
+
